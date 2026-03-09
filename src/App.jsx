4ebe627b58b23ba0e1d6a00d53147b9bf5aa9373
neco <lightz0217@gmail.com>
2026-03-09 07:46:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import bgImage from "./assets/casa.jpg";
+import pagelogo from "./assets/nexora.png";
+
+export default function App() {
+  return (
+    <div className="landing" style={{ backgroundImage: `url(${bgImage})` }}>
+      
+      {/* MENU SUPERIOR */}
+      <div className="header">
+        <img src={pagelogo} width="120" />
+
+        <div className="infos">
+          <a href="#">venda</a>
+          <a href="#">informações</a>
+          <a href="#">contatos</a>
+        </div>
+
+        <button
+          className="compra-ingresso"
+          onClick={() =>
+            window.open(
+              "https://store.steampowered.com/app/570/Dota_2/",
+              "_blank"
+            )
+          }
+        >
+          agendar
+        </button>
+      </div>
+
+      {/* CONTEÚDO PRINCIPAL */}
+      
+      
+    </div>
+  );
+}
